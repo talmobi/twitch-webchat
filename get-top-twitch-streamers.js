ad940fb921de3249bdde251d4e1bd6dc42249656
Christian Talmo <talmo.christian@gmail.com>
2017-02-13 16:56:06 +0200

    utility script to fetch current most popular twitch channels

---
+++ get-top-twitch-streamers.js
@@ -0,0 +1,57 @@
+var path = require('path')
+var childProcess = require('child_process')
+
+// locate phantomjs prebuilt binary
+var phantomjs = require('phantomjs-prebuilt')
+var binPath = phantomjs.path
+
+var childArgs = [
+  '--web-security=no', // enable xss (required for twitch chat)
+  path.join(__dirname, 'get-top-twitch-streamers.phantom.js')
+];
+
+exports.fetch = function (callback) {
+  // create the child process spawn
+  var spawn = childProcess.spawn(binPath, childArgs);
+
+  process.on('close', cleanup)
+  process.on('exit', cleanup)
+  process.on('error', cleanup)
+
+  var buffer = ''
+  spawn.stdout.on('data', function (data) {
+    buffer += data.toString()
+  })
+
+  spawn.on('close', function () {
+    console.log('spawn close')
+    console.log('buffer: ' + buffer.trim())
+    try {
+      var channels = JSON.parse(buffer)
+      console.log('top channel: ' + channels[0])
+      callback(undefined, channels)
+    } catch (err) {
+      callback(err)
+    }
+  })
+
+  spawn.stderr.on('data', function (err) {
+    callback(err)
+    cleanup()
+  })
+
+  function cleanup () {
+    console.log('cleaning up')
+    try {
+      spawn.kill()
+    } catch (err) {}
+    try {
+      process.kill(spawn)
+    } catch (err) {}
+  }
+}
+
+exports.fetch(function (err, channels) {
+  if (err) throw err
+  console.log(channels)
+})
---
+++ get-top-twitch-streamers.phantom.js
@@ -0,0 +1,45 @@
+/* This is a phantomjs script and needs to be run with the phantomjs binary */
+var page = require('webpage').create();
+var $ = require('jquery');
+page.viewportSize = { width: 1, height: 1 };
+
+var _address = 'https://www.twitch.tv/directory/all'
+
+var _timeout = setTimeout(function () {
+  phantom.exit(1)
+}, 10000)
+
+page.open(_address, function (status) {
+  clearTimeout(_timeout)
+  if (status !== 'success') {
+    phantom.exit(1)
+  } else {
+    // var body = page.evaluate(function () { return document.innerHTML })
+
+    var channels = page.evaluate(function () {
+      var dict = {}
+      var anchors = document.querySelectorAll('#directory-list .content .thumb a')
+      for (var i = 0; i < anchors.length; i++) {
+        try {
+          var a = anchors[i]
+          if (a && a.href) dict[a.href] = decodeURI(a.href)
+        } catch (err) {}
+      }
+      return Object.keys(dict).map(function (key) { return dict[key] })
+    })
+
+    if (channels) {
+      channels = channels.map(function (channel) {
+        while (channel[channel.length - 1] === '/') {
+          channel = channel.substr(0, channel.length - 1)
+        }
+        channel = channel.split('/').reverse()[0]
+        return channel
+      })
+      console.log(JSON.stringify(channels))
+      phantom.exit(0)
+    } else {
+      phantom.exit(1)
+    }
+  }
+})
