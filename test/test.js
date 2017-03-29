73a135c62ab3cdd7dda8c4f81e4a1b94a773fe62
Christian Talmo <talmo.christian@gmail.com>
2017-03-30 00:25:52 +0300

    sped up test by sharing result from first test with second test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ var tw = require('../index.js')
 
 var test = require('tape')
 
+var _topChannels
+
 test('Get list of top live streamers', { timeout: 30 * 1000 }, function (t) {
   t.plan(5)
   tw.getTopStreamers(function (err, channels) {
@@ -10,45 +12,44 @@ test('Get list of top live streamers', { timeout: 30 * 1000 }, function (t) {
     t.ok(channels.length > 0, 'non-empty array')
     t.equal(typeof channels[0], 'string')
     t.ok(channels[0].length > 0, 'found top channel: ' + channels[0])
+    _topChannels = channels.slice()
   })
 })
 
 test('Get chat messages from top live streamer', { timeout: 60 * 1000 }, function (t) {
   t.plan(9)
 
-  tw.getTopStreamers(function (err, channels) {
-    var channel = channels[0]
-    t.ok(channel && channel.length > 0, 'found top channel: ' + channel)
+  var channel = _topChannels[0]
+  t.ok(channel && channel.length > 0, 'using top channel from previous test: ' + channel)
 
-    var messages = []
-    var count = 0
-    var ctrl = tw.start(channel, function (err, message) {
-      messages.push(message)
-      count++
-      if (count === 20) {
-        ctrl.kill()
-        setTimeout(function () {
-          check()
-        }, 1500)
-      }
-    })
-
-    function check () {
-      var ticks = messages.filter(function (message) { return message.type === 'tick' })
-      messages = messages.filter(function (message) { return message.type !== 'tick' })
-      var chats = messages.filter(function (message) { return message.type === 'chat' })
-      t.ok(ticks.length > 0, 'tick messages found')
-      t.equal(messages[0].text, 'creating spawn...')
-      t.equal(messages[1].text, 'creating page...')
-      t.equal(messages[2].text, 'opening page...')
-      t.equal(messages[3].text, 'page opened and ready')
-      t.ok(
-        chats[0].from.toLowerCase() === 'jtv' &&
-        chats[0].text.toLowerCase().indexOf('welcome') !== -1,
-        'jtv welcome message found'
-      )
-      t.ok(chats[1].from.length > 0 && chats[1].html.length > 0, 'user message found.')
-      t.equal(messages[messages.length - 1].type, 'exit')
+  var messages = []
+  var count = 0
+  var ctrl = tw.start(channel, function (err, message) {
+    messages.push(message)
+    count++
+    if (count === 20) {
+      ctrl.kill()
+      setTimeout(function () {
+        check()
+      }, 1500)
     }
   })
+
+  function check () {
+    var ticks = messages.filter(function (message) { return message.type === 'tick' })
+    messages = messages.filter(function (message) { return message.type !== 'tick' })
+    var chats = messages.filter(function (message) { return message.type === 'chat' })
+    t.ok(ticks.length > 0, 'tick messages found')
+    t.equal(messages[0].text, 'creating spawn...')
+    t.equal(messages[1].text, 'creating page...')
+    t.equal(messages[2].text, 'opening page...')
+    t.equal(messages[3].text, 'page opened and ready')
+    t.ok(
+      chats[0].from.toLowerCase() === 'jtv' &&
+      chats[0].text.toLowerCase().indexOf('welcome') !== -1,
+      'jtv welcome message found'
+    )
+    t.ok(chats[1].from.length > 0 && chats[1].html.length > 0, 'user message found.')
+    t.equal(messages[messages.length - 1].type, 'exit')
+  }
 })
