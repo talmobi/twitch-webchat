2bbf7929d6fb7bba8d044e44eede3c48d5dcbdb5
Christian Talmo <talmo.christian@gmail.com>
2018-06-23 01:52:44 +0300

    updated demo

--- demo/server-demo.js
+++ demo/server-demo.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('kiite')(server);
 
 var tw = require('../index.js');
 
